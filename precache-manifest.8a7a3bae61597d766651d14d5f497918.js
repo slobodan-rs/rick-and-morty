8a9ded5ae55bd2c36f7b8eecf58e94809c139239
Slobodan <mr.slobodan@hotmail.rs>
2020-09-24 21:05:12 +0200

    Updates

---
+++ precache-manifest.8a7a3bae61597d766651d14d5f497918.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb6422b3f2fd5cc7451beeb6be73b03",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "71af988ca4c881e1c3e1",
+    "url": "/rick-and-morty/static/css/2.70e35f80.chunk.css"
+  },
+  {
+    "revision": "71af988ca4c881e1c3e1",
+    "url": "/rick-and-morty/static/js/2.ee10d839.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty/static/js/2.ee10d839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b43a835362a8008f884",
+    "url": "/rick-and-morty/static/js/main.fbe16f16.chunk.js"
+  },
+  {
+    "revision": "8ec831075791f93706ea",
+    "url": "/rick-and-morty/static/js/runtime-main.2b22dfe8.js"
+  },
+  {
+    "revision": "3d0e3808e2fce5f786d4093038d7825c",
+    "url": "/rick-and-morty/static/media/landing-background.3d0e3808.png"
+  },
+  {
+    "revision": "b3594ee4e4f3d97d0216b6ad958d0b32",
+    "url": "/rick-and-morty/static/media/logo.b3594ee4.png"
+  },
+  {
+    "revision": "5b537363393dc26c68b566fe482eb32d",
+    "url": "/rick-and-morty/static/media/portal.5b537363.png"
+  },
+  {
+    "revision": "9073b990d6c35fcf32b66b3f524fc019",
+    "url": "/rick-and-morty/static/media/season-1.9073b990.png"
+  },
+  {
+    "revision": "72c98a279e119f6b9ba7c22fc03cb91e",
+    "url": "/rick-and-morty/static/media/season-2.72c98a27.png"
+  },
+  {
+    "revision": "f8d000aff8f3f408c4724b15384154f8",
+    "url": "/rick-and-morty/static/media/season-3.f8d000af.png"
+  },
+  {
+    "revision": "3c62af8739263dd0ba62902fcc2b3f5e",
+    "url": "/rick-and-morty/static/media/season-4.3c62af87.png"
+  },
+  {
+    "revision": "1608dfebccca8644a01eb1f67115d308",
+    "url": "/rick-and-morty/static/media/title.1608dfeb.png"
+  }
+]);
